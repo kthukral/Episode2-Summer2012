7d6e795b861315df35d8615f957a00ab66a795c5
Karan Thukral <karanthukral@gmail.com>
2013-11-27 17:24:49 -0500

    finished 1st level. Made bug while doing average ratings

--- movie_json.rb
+++ movie_json.rb
@@ -1,11 +1,38 @@
 require_relative "lib/movie"
 require_relative "lib/api"
 
+movies_added = Array.new
+
 def find_movie
-  puts "OH HAI. Search?"
+  puts "Add a movie you really like"
   movie_title = gets
+  if movie_title == "\n"
+    puts "Please enter a title"
+    find_movie
+  end
   movie = Api.search_by_title(movie_title)
-  puts "Found: #{movie.title}. Score: #{movie.score}"
+  if movie
+    #puts "Found: #{movie.title}. Score: #{movie.score}"
+    puts "\nFound:\n"
+    puts "Title -> #{movie.title}"
+    puts "Score -> #{movie.score}"
+    puts "Year -> #{movie.year}"
+    movies_added << movie
+    count = 0 
+    totalScores = 0
+    for moviefound in movies_added
+      score = moviefound.score
+      totalScores += score
+      count++
+    end
+    avg = totalScores/count
+
+    puts "\n\nAverge Rattings of all movies added = #{avg}"
+    
+  else
+    puts "Sorry No Movie with that Title Was Found."
+  end
+    
 end
 
 find_movie
